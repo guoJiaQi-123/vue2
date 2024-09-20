d6b99fa2206195bfb2d2531d64e9d3892439ce00
GuoJiaqi <2667629684@qq.com>
2024-09-20 23:54:32 +0800

    add服务端代码上传

---
+++ server/src/main/java/com/itheima/App.java
@@ -0,0 +1,37 @@
+package com.itheima;
+
+import com.itheima.controller.LoginInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@Slf4j
+public class App implements WebMvcConfigurer {
+
+    @Bean
+    public LoginInterceptor loginInterceptor() {
+        return new LoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor()).addPathPatterns("/api/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:9999")
+                .allowCredentials(true);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        log.info("******************* 项目启动成功 *******************");
+    }
+}
