68d245dfc9ae54aa9e610c025b64709251850521
GuoJiaqi <2667629684@qq.com>
2024-09-23 22:34:59 +0800

    add VUEX多组件之间共享数据【响应式】

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,29 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+import axios from "../utils/myaxios";
 
 export default new Vuex.Store({
-    state: {},
+    state: {
+        name: '',
+        age: 18
+    },
     getters: {},
-    mutations: {},
-    actions: {},
+    mutations: {
+        updateName(state, name) {
+            state.name = name;
+        },
+        updateServeUser(state, user) {
+            const {name, age} = user;
+            state.name = name;
+            state.age = age;
+        }
+    },
+    actions: {
+        async updateServeUser(context) {
+            const resp = await axios().get('/api/user');
+            context.commit('updateServeUser', resp.data.data)
+        }
+    },
     modules: {}
 })
